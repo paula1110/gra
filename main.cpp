4f423a3daf73721013e5ee08620930d62cccb757
paula1110 <paulasalama673@gmail.com>
2026-06-05 12:21:21 +0200

    5czerwca

--- main.cpp
+++ main.cpp
@@ -58,31 +58,54 @@ bool check(int r,int c,char maz[10][10])
     else
         return true;
 }
-
+bool checkwin(int r,int c,char maz[10][10])
+{
+    if(maz[r][c]=='E')
+        return true;
+    else
+        return false;
+}
 int main(){
 
-    int pr=1,pc=0;
+    int pr=1,pc=0,l=1;
     char x;
     char (*actual)[10]=maz1;
     while(true)
     {
+
         system("cls");
-        printmaz(maz1);
+        printmaz(actual);
         cin>>x;
         if(x=='w')
         {
             pr--;
             if(check(pr,pc,actual)== 1)
             {
+
                 swap(actual[pr+1][pc],actual[pr][pc]);
             }
             else
             {
                 pr++;
                 cout<<" you cant go there";
-                this_thread::sleep_for(chrono::seconds(5));
+                this_thread::sleep_for(chrono::seconds(2));
                 continue;
             }
+             if(checkwin(pr+1,pc,actual)==1)
+        {
+            cout<<"you win";
+            //this_thread::sleep_for(chrono::seconds(2));
+            pr=1;
+            pc=0;
+            l++;
+            if(l==2)
+               actual=maz2;
+            else
+                actual=maz3;
+
+
+        }
+
         }
         else if(x=='s')
         {
@@ -94,9 +117,23 @@ int main(){
             else
             {pr--;
                 cout<<" you cant go there";
-                this_thread::sleep_for(chrono::seconds(5));
+                this_thread::sleep_for(chrono::seconds(2));
                 continue;
             }
+             if(checkwin(pr-1,pc,actual)==1)
+        {
+            cout<<"you win";
+            pr=1;
+            pc=0;
+            l++;
+            if(l==2)
+               actual=maz2;
+            else
+                actual=maz3;
+
+
+        }
+
         }
         else if(x=='a')
         {
@@ -109,9 +146,23 @@ int main(){
             {
                 pc++;
                 cout<<" you cant go there";
-                this_thread::sleep_for(chrono::seconds(5));
+                this_thread::sleep_for(chrono::seconds(2));
                 continue;
             }
+             if(checkwin(pr,pc+1,actual)==1)
+        {
+            pr=1;
+            pc=0;
+            cout<<"you win";
+            l++;
+            if(l==2)
+               actual=maz2;
+            else
+                actual=maz3;
+
+
+        }
+
         }
         else if(x=='d')
         {
@@ -123,11 +174,30 @@ int main(){
             else
             {pc--;
                 cout<<" you cant go there";
-                this_thread::sleep_for(chrono::seconds(5));
+                this_thread::sleep_for(chrono::seconds(2));
                 continue;
             }
+             if(checkwin(pr,pc-1,actual)==1)
+        {
+            cout<<"you win";
+            pr=1;
+
+            pc=0;
+            l++;
+            if(l==2)
+               actual=maz2;
+            else if(l==3)
+                actual=maz3;
+            else
+                break;
+
+
+        }
+
         }
 
     }
+    system("cls");
+    cout<<"gratulacje, wygrales";
 
 }
